c1a985670c64654eb871a4764ee138b409767f21
Ilya Radchenko <knownasilya@gmail.com>
2020-04-02 20:36:33 -0400

    signin form

--- src/authenticated.tsx
+++ src/authenticated.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import * as firebase from 'firebase/app';
 
 interface AuthenticatedInput {
   user: object;
 }
 
 const Authenticated: React.FC<AuthenticatedInput> = ({ user }) => (
-  <div className="m-4">{JSON.stringify(user)}</div>
+  <div className="m-4">
+    {JSON.stringify(user)}
+    <br />
+    <button
+      onClick={() => {
+        firebase.auth().signOut();
+      }}
+    >
+      Sign Out
+    </button>
+  </div>
 );
 
 export default Authenticated;
--- src/unauthenticated.tsx
+++ src/unauthenticated.tsx
@@ -1,9 +1,49 @@
 import * as React from 'react';
+import * as firebase from 'firebase/app';
 
 interface UnauthenticatedInput {}
+interface SigninFormElements {
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+}
+type SigninElements = SigninFormElements & HTMLFormControlsCollection;
 
-const Unauthenticated: React.FC<UnauthenticatedInput> = () => (
-  <div className="m-4">Login...</div>
-);
+const Unauthenticated: React.FC<UnauthenticatedInput> = () => {
+  function handleSignIn(event: React.SyntheticEvent) {
+    event.preventDefault();
+    let target: HTMLFormElement = event.target as HTMLFormElement;
+    let elements = target.elements as SigninElements;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        elements.email.value,
+        elements.password.value
+      );
+  }
+
+  return (
+    <div className="m-4">
+      <form onSubmit={handleSignIn}>
+        <div>
+          <label className="block" htmlFor="email">
+            Email
+          </label>
+          <input className="border" type="email" id="email" />
+        </div>
+
+        <div>
+          <label className="block" htmlFor="password">
+            Password
+          </label>
+          <input className="border" type="password" id="password" />
+        </div>
+
+        <button className="p-2 bg-gray-300" type="submit">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Unauthenticated;
