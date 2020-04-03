b1bc4dcf1768d70a0ef80028958a685b8d3fc37f
Ilya Radchenko <knownasilya@gmail.com>
2020-04-03 10:44:57 -0400

    styles

--- src/unauthenticated.tsx
+++ src/unauthenticated.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import * as firebase from 'firebase/app';
 
-interface UnauthenticatedInput {}
 interface SigninFormElements {
   email: HTMLInputElement;
   password: HTMLInputElement;
 }
 type SigninElements = SigninFormElements & HTMLFormControlsCollection;
 
-const Unauthenticated: React.FC<UnauthenticatedInput> = () => {
+const SignIn: React.FC<{}> = () => {
   function handleSignIn(event: React.SyntheticEvent) {
     event.preventDefault();
     let target: HTMLFormElement = event.target as HTMLFormElement;
@@ -22,39 +21,50 @@ const Unauthenticated: React.FC<UnauthenticatedInput> = () => {
   }
 
   return (
-    <div className="m-4">
-      <form onSubmit={handleSignIn}>
-        <div>
-          <label className="block" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            type="email"
-            id="email"
-          />
-        </div>
+    <form
+      className="max-w-sm rounded overflow-hidden shadow-lg p-5"
+      onSubmit={handleSignIn}
+    >
+      <h2 className="text-lg font-semibold">Sign In</h2>
 
-        <div className="mt-2">
-          <label className="block" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            type="password"
-            id="password"
-          />
-        </div>
+      <div className="mt-4">
+        <label className="block" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="email"
+          id="email"
+        />
+      </div>
 
-        <div className="mt-2">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Sign In
-          </button>
-        </div>
-      </form>
+      <div className="mt-4">
+        <label className="block" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="password"
+          id="password"
+        />
+      </div>
+
+      <div className="mt-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Unauthenticated: React.FC<{}> = () => {
+  return (
+    <div className="m-4 grid grid-cols-2 gap-1">
+      <SignIn />
     </div>
   );
 };
